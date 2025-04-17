924f8a40070d749d633ce1d0715eaab488d504d7
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-04-17 11:31:52 -0700

    Added text color.

--- my-react-app/src/components/Recipe.jsx
+++ my-react-app/src/components/Recipe.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import { Typography, Box} from '@mui/material'
+import { Typography, Box } from '@mui/material'
 
 export function Recipe({ recipe }) {
     if (!recipe) return null;
     
     return (
-        <Box sx={{ p: 2, m: 2, maxWidth: 600, mx: 'auto' }}>
-          <Typography variant="h4" gutterBottom>
+        <Box sx={{ p: 2, m: 2, maxWidth: 600, mx: 'auto',}}>
+          <Typography variant="h4" color='pink' gutterBottom>
             {recipe.name}
           </Typography>
     
-          <Typography variant="subtitle1">Ingredients</Typography>
+          <Typography variant="subtitle1" color='pink'>Ingredients</Typography>
           <ul>
             {recipe.ingredients.map((ing, idx) => (
               <li key={idx}>{ing}</li>
             ))}
           </ul>
     
-          <Typography variant="subtitle1">Instructions</Typography>
+          <Typography variant="subtitle1" color='pink'>Instructions</Typography>
           <ol>
             {recipe.instructions.map((step, idx) => (
               <li key={idx}>{step}</li>
